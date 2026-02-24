f7fc277d5afca16eaf026bd895ba14fced40d96e
Tarun <karannp7779@gmail.com>
2026-02-25 00:05:52 +0530

    Movie detail page some error

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,16 +3,16 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full text-gray-300">
-      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
+    <footer className="px-6 md:px-16 lg:px-36 mt-40 w-full text-gray-300">
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14">
         <div className="md:max-w-96">
           <img alt="" class="h-11" src={assets.logo} />
           <p className="mt-6 text-sm">
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
           </p>
           <div className="flex items-center gap-2 mt-4">
-            <img src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/googlePlayBtnBlack.svg" alt="google play" className="h-10 w-auto border border-white rounded" />
-            <img src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/appleStoreBtnBlack.svg" alt="app store" className="h-10 w-auto border border-white rounded" />
+            <img src={assets.googlePlay} alt="google play" className="h-10 w-auto border border-white rounded" />
+            <img src={assets.appStore} alt="app store" className="h-10 w-auto border border-white rounded" />
           </div>
         </div>
         <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
--- client/src/pages/Moviedetails.jsx
+++ client/src/pages/Moviedetails.jsx
@@ -1,13 +1,49 @@
-import React from "react";
-//import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
+import BlurCircle from "../components/BlurCircle";
+import { StarIcon } from "lucide-react";
+import timeFormat from "../lib/timeFormat";
 
 const Moviedetails = () => {
-  // const { id } = useParams()
-  // const [show, setShow] = useState(null)
-  return (
-    <div>
+  const { id } = useParams()
+  const [show, setShow] = useState(null)
 
+  useEffect(() => {
+    const getShow = async () => {
+      const show = dummyShowsData.find(show => show._id === id)
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData
+      })
+    }
+    getShow()
+  }, [id])
+
+  return show ? (
+    <div className="px-16 md:px-16 lg:px-40 pt-30 md:pt-50">
+      <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
+        <img src={show.movie.poster_path} alt="" className="max-md:mx-auto rounded-xl h-104 max-w-70 object-cover" />
+        <div className="relative flex flex-col gap-3">
+          <BlurCircle top="-100px" left="-100px" />
+          <p className="text-primary">ENGLISH</p>
+          <h1 className="text-4xl font-semibold max-w-96 text-balance">{show.movie.title}</h1>
+          <div className="flex items-center gap-4 text-gray-300">
+            <StarIcon className="w-5 h-5 text-primary fill-primary" />
+            {show.movie.vote_average.toFixed(1)} User Rating
+
+          </div>
+          <p className="text-gray-400 mt-2 text-sm leading-">{show.movie.overview}</p>
+
+          <p>
+            {timeFormat(show.movie.runtime)} ◉ {show.movie.genres.map(genre => genre.name).join(" , ")} ◉ {show.movie.release_date.split("-")[0]}
+          </p>
+
+        </div>
+      </div >
     </div>
+  ) : (
+    <div>Loading...</div>
   );
 }
 
