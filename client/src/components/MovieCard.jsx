3dde10e86129fbb3dfc7708b6ccfbdf6bda4b5a1
Tarun Prajapati <karannp7779@gmail.com>
2026-05-24 18:36:18 +0530

    MOVIE Ditile page ready

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ const MovieCard = ({ movie }) => {
 
     return (
         <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66'>
-            <img onClick={() => { navigate(`/movie/${movie._id}`); scrollTo(0, 0) }}
+            <img onClick={() => { navigate(`/movies/${movie._id}`); scrollTo(0, 0) }}
                 src={movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-bottom-right cursor-pointer' />
             <p className='font-semibold mt-2 truncate'>{movie.title}</p>
 
@@ -16,7 +16,7 @@ const MovieCard = ({ movie }) => {
                 {new Date(movie.release_date).getFullYear()} ◉ {movie.genres.slice(0, 2).map(genre => genre.name).join(" | ")} ◉ {timeFormat(movie.runtime)}
             </p>
             <div className='flex items-center justify-between mt-4 pb-3'>
-                <button onClick={() => { navigate(`/movie/${movie._id}`); scrollTo(0, 0) }} className='flex items-center gap-2 px-8 py-3.5 text-sm bg-linear-to-r from-pink-500 to-pink-400 hover:from-pink-600 hover:to-pink-500 transition-all duration-300 rounded-full font-semibold cursor-pointer shadow-lg shadow-pink-500/30 hover:shadow-pink-500/50 hover:scale-105'>Buy Tickets</button>
+                <button onClick={() => { navigate(`/movies/${movie._id}`); scrollTo(0, 0) }} className='flex items-center gap-2 px-8 py-3.5 text-sm bg-linear-to-r from-pink-500 to-pink-400 hover:from-pink-600 hover:to-pink-500 transition-all duration-300 rounded-full font-semibold cursor-pointer shadow-lg shadow-pink-500/30 hover:shadow-pink-500/50 hover:scale-105'>Buy Tickets</button>
                 <p className='flex items-center gap-1 text-gray-400 mt-1 pr-1' >
                     <StarIcon className="w-4 h-4 text-primary fill-primary" />
                     {movie.vote_average.toFixed(1)}
--- client/src/pages/Moviedetails.jsx
+++ client/src/pages/Moviedetails.jsx
@@ -4,6 +4,8 @@ import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import BlurCircle from "../components/BlurCircle";
 import { StarIcon } from "lucide-react";
 import timeFormat from "../lib/timeFormat";
+import { PlayCircleIcon } from "lucide-react";
+import { Heart } from "lucide-react";
 
 const Moviedetails = () => {
   const { id } = useParams()
@@ -33,14 +35,41 @@ const Moviedetails = () => {
             {show.movie.vote_average.toFixed(1)} User Rating
 
           </div>
-          <p className="text-gray-400 mt-2 text-sm leading-">{show.movie.overview}</p>
+          <p className="text-gray-400 mt-2 text-sm leading-6">{show.movie.overview}</p>
 
           <p>
             {timeFormat(show.movie.runtime)} ◉ {show.movie.genres.map(genre => genre.name).join(" , ")} ◉ {show.movie.release_date.split("-")[0]}
           </p>
 
+          <div className="flex items-center flex-wrap gap-4 mt-4">
+            <button className=" flex items-center gap-2 px-7 py-3 text-sm bg-red-800 hover:bg-gray-900 transition rounded-md fron-mideum cursor-pointer active:scale-95">
+              <PlayCircleIcon className="w-5 h-5" />
+              Watch Trailer
+            </button>
+            <a href="#dateSelect" className="flex items-center gap-2 px-7 py-3 text-sm bg-linear-to-r from-pink-500 to-pink-400 hover:from-pink-600 hover:to-pink-500 transition-all duration-300 rounded-md font-semibold cursor-pointer shadow-lg shadow-pink-500/30 hover:shadow-pink-500/50 hover:scale-105">Buy Tickets</a>
+            <button className="bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95">
+              <Heart className={`w-5 h-5`} />
+            </button>
+          </div>
+
+
+
         </div>
       </div >
+
+      <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
+      <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+        <div className="flex items-center gap-4 w-max px-4">
+          {show.movie.casts.slice(0, 12).map((cast, index) => (
+            <div key={index} className="flex flex-col items-center text-center">
+              <img src={cast.profile_path} alt="" className="rounded-full h-20 md:h-20 aspect-square object-cover" />
+              <p>{cast.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+
     </div>
   ) : (
     <div>Loading...</div>
